e548f98f97b51cc16b84ac3b39c2f83ca7e18759
Jorge Covello <jorgejfcovello@gmail.com>
2023-04-19 16:01:07 -0300

    fix

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-// como se maneja la concurrencia en javascript?
-// Event loop
-/*
-index
-inicio de loop
-    setTimeout/SetInterval
-    js
-    network/disk/process
-    js
-    setImmediate
-    js
-    close events
-    js
-fin de loop
-exit
-*/
 
-setTimeout(() => console.log('Hola'), 3000)
-setTimeout(() => {
-    console.log('1'), 1000
-    setTimeout(() => {
-        console.log('2')
-        setTimeout(() => console.log('3'), 1000)
-    }, 1000)
-})
+setTimeout(() => console.log('Hola'), 0)
+setTimeout(() => {console.log('1')}, 1000)
+setTimeout(() => {console.log('2')}, 2000)
+setTimeout(() => {console.log('3')}, 3000)
+console.log('test')
+
